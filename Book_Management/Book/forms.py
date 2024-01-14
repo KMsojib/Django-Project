075f95df51c635f530e19fa9b73714a30fb30f54
MD. Kawsar Mahmud <119809945+KMsojib@users.noreply.github.com>
2024-01-14 22:48:05 +0600

    Delete Book_Management directory

--- Book_Management/Book/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from Book.models import BookStoreModel
-
-class BookStoreForm(forms.ModelForm):
-    
-    class Meta:
-        model = BookStoreModel
-        exclude = ['First_publication','Last_publication']
